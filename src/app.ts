d19635f920f1e34110bc6865faa2b9e2835d0847
Gunji <justgunji@gmail.com>
2024-02-24 16:03:23 +0100

    setup docker

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("Hello World");
+import express, { Application } from "express";
+
+const app: Application = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const port: number = Number(process.env.PORT) || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
